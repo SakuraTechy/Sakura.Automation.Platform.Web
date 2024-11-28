e53f315d677e879a9eee6a613014c38e70361eea
liuzhi <1306086303@qq.com>
2024-11-28 17:01:05 +0800

    feat(检查操作)：优化options，新增checklist提示示例

--- src/utils/options.js
+++ src/utils/options.js
@@ -883,7 +883,9 @@ export const operations = [
         id: 'web-checklist',
         name: '检查从数据库中查询出的结果中的值',
         config: [
-          { paramsName: 'details', paramsValue: `condition:Proportion1;subject:xxxx`},
+          { paramsName: 'details', paramsValue: `condition:size_not_null;subject:${key}`},
+          { paramsName: 'details', paramsValue: `condition:field;subject:${key}`},
+          { paramsName: 'details', paramsValue: `condition:field;subject:${key[0].IP}`},
           { paramsName: 'expect', paramsValue: `这里填写预期结果`},
           { paramsName: 'message', paramsValue: `这里填写检查失败的提示内容(实际结果和预期结果不一致)`},
           { paramsName: 'skip', paramsValue: `locator（默认locator，可跳过locator和expect，跳过后会标记为成功）`}
@@ -893,7 +895,8 @@ export const operations = [
         id: 'web-checkset',
         name: '检查Web本地缓存的list中的元素',
         config: [
-          { paramsName: 'value', paramsValue: '${test}'},
+          { paramsName: 'value', paramsValue: '${key}'},
+          { paramsName: 'value', paramsValue: '${key[0].IP}'},
           { paramsName: 'expect', paramsValue: '这里填写检查失败的提示内容'},
           { paramsName: 'message', paramsValue: '这里填写检查失败的提示内容(实际结果和预期结果不一致)'},
         ],
@@ -902,7 +905,8 @@ export const operations = [
         id: 'web-notchecklists',
         name: '不检查Web本地缓存的list中的元素',
         config: [
-          { paramsName: 'value', paramsValue: '${test}'},
+          { paramsName: 'value', paramsValue: '${key}'},
+          { paramsName: 'value', paramsValue: '${key[0].IP}'},
           { paramsName: 'expect', paramsValue: '这里填写检查失败的提示内容'},
           { paramsName: 'message', paramsValue: '这里填写检查失败的提示内容(实际结果和预期结果不一致)'},
         ],
